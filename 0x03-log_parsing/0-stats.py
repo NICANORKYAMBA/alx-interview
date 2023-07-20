460470bc1376e537c64a804b55dbd32dca2fa10a
NICANORKYAMBA <nicanorkyamba98@gmail.com>
2023-07-20 15:36:41 +0300

    Fixed code failure

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -14,21 +14,14 @@ STATUSES = {'200': 0, '301': 0, '400': 0,
 
 def print_stats(file_size, status_counts):
     """
-    Prints the status code and the number of requests for
-    that code
+    Print the number of status code and the file size
 
-    Parameters
-    ----------
-    file_size : int
-        The size of the file in bytes
-    status_counts : dict
-        A dictionary where the keys are the status codes
-        and the values are the number of requests for
-        that code
+    Args:
+        file_size (int): file size
+        status_counts (dict): status counts
 
-    Returns
-    -------
-    None
+    Returns:
+        None
     """
     print('File size: {:d}'.format(file_size))
     for status in sorted(status_counts.keys()):
@@ -36,36 +29,28 @@ def print_stats(file_size, status_counts):
             print('{}: {:d}'.format(status, status_counts[status]))
 
 
-def process_line(line, file_size, status_counts):
-    line_parts = line.split()
-    if len(line_parts) != 9:
-        return
-
-    status_code = line_parts[-2]
-    try:
-        file_size += int(line_parts[-1])
-        status_counts[status_code] += 1
-    except ValueError:
-        pass
-
-    return file_size, status_counts
-
-
 if __name__ == '__main__':
     file_size = 0
-    status_counts = STATUSES.copy()
     line_count = 0
+    status_counts = STATUSES.copy()
 
     try:
         for line in sys.stdin:
-            file_size, status_counts = process_line(
-                    line.strip(), file_size, status_counts)
-            line_count += 1
+            line_parts = line.split()
+            if len(line_parts) != 9:
+                continue
 
+            status_code = line_parts[-2]
+            try:
+                file_size += int(line_parts[-1])
+                status_counts[status_code] += 1
+            except ValueError:
+                pass
+
+            line_count += 1
             if line_count % 10 == 0:
                 print_stats(file_size, status_counts)
 
-        print_stats(file_size, status_counts)
-
     except KeyboardInterrupt:
         print_stats(file_size, status_counts)
+        raise
