e6071733fc95e76e89da47b2193798ce97c174a0
NICANORKYAMBA <nicanorkyamba98@gmail.com>
2023-08-22 14:29:06 +0300

    Improved code efficiency

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -22,14 +22,16 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
 
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
+    coins.sort(reverse=True)
+
+    count = 0
 
     for coin in coins:
-        for i in range(coin, total + 1):
-            dp[i] = min(dp[i], dp[i - coin] + 1)
+        while total >= coin:
+            total -= coin
+            count += 1
 
-    if dp[total] == float('inf'):
+    if total == 0:
+        return count
+    else:
         return -1
-
-    return dp[total]
