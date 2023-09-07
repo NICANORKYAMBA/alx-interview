eb3bd628c268fc88b9b923efa476337d6ef7cfef
NICANORKYAMBA <nicanorkyamba98@gmail.com>
2023-09-07 16:09:44 +0300

    Fixed code failure

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -7,54 +7,63 @@ Created on Thur Sep  7 10:00:00 2023
 """
 
 
-def is_prime(n):
-    """
-    Function to check if a number is prime
-
-    Args:
-        n (int): number to check
-
-    Returns:
-        bool: True if prime, False if not
-    """
-    if n <= 1:
-        return False
-    if n <= 3:
-        return True
-    if n % 2 == 0 or n % 3 == 0:
-        return False
-    i = 5
-    while i * i <= n:
-        if n % i == 0 or n % (i + 2) == 0:
-            return False
-        i += 6
-    return True
-
-
 def isWinner(x, nums):
     """
-    Function to check if Maria or Ben wins the game
+    Find the winner of the game.
 
     Args:
-        x (int): number of rounds
-        nums (list): list of numbers
+        x (int): The number of rounds
+        nums (list): The list of numbers
 
     Returns:
-        str: "Maria" or "Ben" or None
+        str: The winner
     """
-    if x <= 0 or not nums:
+    def sieve_eratosthenes(limit):
+        """
+        Sieve of Eratosthenes.
+
+        Args:
+            limit (int): The limit of the sieve
+
+        Returns:
+            list: The list of primes
+        """
+        sieve = [True] * (limit + 1)
+        sieve[0] = sieve[1] = False
+        primes = []
+
+        for current in range(2, limit + 1):
+            if sieve[current]:
+                primes.append(current)
+                for multiple in range(current * current, limit + 1, current):
+                    sieve[multiple] = False
+
+        return primes
+
+    def can_win(round_num, primes):
+        """
+        Check if Maria can win.
+
+        Args:
+            round_num (int): The round number
+            primes (list): The list of primes
+
+        Returns:
+            bool: True if Maria can win, False otherwise
+        """
+        if round_num % 2 == 0:
+            return False
+        for prime in primes:
+            if round_num % prime == 0:
+                return True
+        return False
+
+    if max(nums) <= 1:
         return None
 
-    ben_wins = 0
-    maria_wins = 0
-
-    for n in nums:
-        prime_count = sum(1 for i in range(1, n + 1) if is_prime(i))
-
-        if prime_count % 2 == 0:
-            ben_wins += 1
-        else:
-            maria_wins += 1
+    primes = sieve_eratosthenes(max(nums))
+    maria_wins = sum(1 for round_num in nums if can_win(round_num, primes))
+    ben_wins = len(nums) - maria_wins
 
     if maria_wins > ben_wins:
         return "Maria"
